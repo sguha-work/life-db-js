189593b66d1395f6b1203be2a96031f5589e5b0b
sguha-work <sguha1988.life@gmail.com>
2016-03-01 12:14:39 +0530

    changes

--- life-db.js
+++ life-db.js
@@ -1,17 +1,18 @@
 var LifeDB = (function() {
 	// private properties
-	var databaseName, // holds the name of the database
-		insertIntoDataBase, // the first core method to be hitted in time of inserting data to database
-		showErrorMessage, // this method display error in console
-		errorMessages, // this variables holds all kind of error messagesl
-		checkAndSetGlobalStorage, // this method will check wheather sessionStorage is availeble for storing data
+	var databaseName, // Holds the name of the database
+		insertIntoDataBase, // The first core method to be hitted in time of inserting data to database
+		showErrorMessage, // This method display error in console
+		errorMessages, // This variables holds all kind of error messagesl
+		checkAndSetGlobalStorage, // This method will check wheather sessionStorage is availeble for storing data
 		isSessionStorageAvaileble, // If set then session storage is availeble for storing data
 		isNodeEnvironment, // If set then the environment is node js
-		checkIfNodeEnvironment, // this method set the isNodeEnvironment flag
-		globalDataStorage, // this variable will be used as data storage
-		backUpData, // backup the data in sessionStorage or in file
-		checkAndRestoreData; // this method checks if there is any backup exists and restore the data to database
-
+		checkIfNodeEnvironment, // This method set the isNodeEnvironment flag
+		globalDataStorage, // This variable will be used as data storage
+		backUpData, // Backup the data in sessionStorage or in file
+		checkAndRestoreData, // This method checks if there is any backup exists and restore the data to database
+		checkIfPageExists, // This method checks if the page exists or not
+		filterRecords; // This method filtered the records based on query object provided
 	// public properties
 	this.initiate;
 	this.insert;
@@ -19,6 +20,32 @@ var LifeDB = (function() {
 	this.query;
 	this.remove;
 
+	/**
+	* @description This method filtered the records based on query object provided
+	* @param pageName {String} - The name of the page where the query will hit
+	* @param queryObject {Object} - The query, Optional, if not provided all of the page data will be returned
+	* @return {Array} - Array of matched records
+	*/
+	filterRecords = (function(pageName, queryObject) {
+
+	});
+
+	/**
+	* @description This method checks if the page exists or not
+	* @param pageName {String} - The name of the page
+	* @returns {Boolean} - Return true if page exists, false instead
+	*/
+	checkIfPageExists = (function(pageName) {
+		if(typeof globalDataStorage[pageName] === "undefined") {
+			return false;
+		} else {
+			return true;
+		}
+	});
+
+	/**
+	* @description This function checks if backup exists or not, if exists then restores the data
+	*/
 	checkAndRestoreData = (function() {
 		var fs;
 		if(!isNodeEnvironment && isSessionStorageAvaileble) {
@@ -44,7 +71,7 @@ var LifeDB = (function() {
 	});
 
 	/**
-	* This public method is called to insert single or multiple data to a perticuler page
+	* @description This public method is called to insert single or multiple data to a perticuler page
 	* @param pageName {String} - The name of the page where the record or records will go
 	* @param record {Object/Array} - The data which are going to be inserted in the page
 	* @return {Number} - The number of rows effected
@@ -73,7 +100,7 @@ var LifeDB = (function() {
 	});
 	
 	/**
-	* This function backup the database to sessionStorage if the environment is browser or backup the database in file
+	* @description This function backup the database to sessionStorage if the environment is browser or backup the database in file
 	* if the environment is Node
 	* 
 	*/
@@ -90,14 +117,16 @@ var LifeDB = (function() {
 	});
 
 	/**
-	* Defining all the error messages with type of the error
+	* @description Defining all the error messages with type of the error
 	*/
 	errorMessages = {
-		"dbNameNotSpecified": "Database name mast have to provide in time of initiating the database"
+		"dbNameNotSpecified": "Database name mast have to provide in time of initiating the database",
+		"pageNameCannotBeEmpty": "Page name required",
+		"pageDoesnotExists": "Provided page doesn't exists"
 	};
 
 	/**
-	* The function responsible for displaying all kind of errors
+	* @description The function responsible for displaying all kind of errors
 	* @param {string} errorType - The type of the error
 	*/
 	showErrorMessage = (function(errorType) {
@@ -105,7 +134,7 @@ var LifeDB = (function() {
 	});
 
 	/**
-	* This function checks wheather the environment is Node environment or not
+	* @description This function checks wheather the environment is Node environment or not
 	*/
 	checkIfNodeEnvironment = (function() {
 		if(this===window) {
@@ -116,7 +145,7 @@ var LifeDB = (function() {
 	});
 
 	/**
-	* This method checks if the sessionStorage is availeble to be used as data storage or not.
+	* @description This method checks if the sessionStorage is availeble to be used as data storage or not.
 	* If availeble then this method set the flag "isSessionStorageAvaileble" as "true" else "false"
 	*/
 	checkAndSetGlobalStorage = (function() {
@@ -129,7 +158,7 @@ var LifeDB = (function() {
 	});
 
 	/**
-	* The initiate function can receives 2 arguments
+	* @description This function instantiated the database. 
 	* @param {string} databaseName - The name of the database
 	*/
 	this.initiate = (function() {
@@ -145,19 +174,42 @@ var LifeDB = (function() {
 	})(arguments);
 
 	/**
-	* This public method is called to insert single or multiple data to a perticuler page
+	* @description This public method is called to insert single or multiple data to a perticuler page
 	* @param pageName {String} - The name of the page where the record or records will go
 	* @param record {Object/Array} - The data which are going to be inserted in the page
 	* @param callBack {Function} - Optional, The callback function which will be called afterthe insert process is complete
+	* @param backupDatabase {Boolean} - Optional, default true, If true then the program will try to backup the data in session storage or file
 	*/
-	this.insert = (function(pageName, record, callBack) {
+	this.insert = (function(pageName, record, callBack, backupDatabase) {
 		var numberOfEffectedRows;
 		numberOfEffectedRows = insertIntoDataBase(pageName, record);
-		backUpData();
+		if(typeof backupDatabase === "undefined" || backupDatabase) {
+			backUpData();
+		}
 		if(!numberOfEffectedRows) {
 			callback(true, 0); // errorOccured, numberOfEffectedRows 0
 		} else {
 			callback(false, numberOfEffectedRows);
+		}
+	});
+
+	/**
+	* @param pageName {String} - The name of the page where the query will hit
+	* @param queryObject {Object} - The query, Optional, if not provided all of the page data will be returned
+	* @return {Array} - Array of matched records
+	* @description This public method is called to filtered the records of a page based on query
+	*/
+	this.query = (function(pageName, queryObject) {
+		if(pageName.trim() === "") {
+			showErrorMessage("pageNameCannotBeEmpty");
+			return false;
+		} else {
+			if(!checkIfPageExists(pageName)) {
+				showErrorMessage("pageDoesnotExists");
+				return false;
+			} else {
+				return filterRecords(pageName, queryObject);
+			}
 		}
 	});
 });
