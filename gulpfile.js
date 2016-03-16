192e620ffaa223f02af45c465cfbb4ddf82e3001
sguha-work <sguha1988.life@gmail.com>
2016-03-17 00:21:41 +0530

    first production

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp'); 
-var uglify = require('gulp-uglify');
-
-
-gulp.task('default', function() {
-  return gulp.src('life-db.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('production'));
-});
